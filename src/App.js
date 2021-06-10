ba4bbaca33ff2330288e54340b0fc1e3ebb3c5bf
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-06-10 12:09:49 -0500

    double click on task toggles reminder

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
-import Header from './components/Header';
-import Tasks from './components/Tasks';
-import {useState} from 'react';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
-const App = () => {  
-
+const App = () => {
   const [tasks, setTasks] = useState([
     {
       id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
       reminder: true,
     },
     {
       id: 2,
-      text: 'Meeting at school',
-      day: 'Feb 6th at 1:30pm',
+      text: "Meeting at school",
+      day: "Feb 6th at 1:30pm",
       reminder: true,
     },
     {
       id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 7th at 2:30pm',
+      text: "Food Shopping",
+      day: "Feb 7th at 2:30pm",
       reminder: false,
-    }
-  ])
+    },
+  ]);
 
   // Delete Task
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle REminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks To Show'}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
-}
+};
 
 export default App;
