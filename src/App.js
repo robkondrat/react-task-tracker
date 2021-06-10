c616eeebf6450b07782e00c7c477d6c9b0cf53d9
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-06-10 12:25:44 -0500

    user can create new task, task appears

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+
 import { useState } from "react";
 
 const App = () => {
@@ -24,6 +26,13 @@ const App = () => {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask])
+  };
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -41,6 +50,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
